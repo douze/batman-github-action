97b19cb0f808615bc992c68390defb81994a5588
douze <douze@users.noreply.github.com>
2023-02-20 21:55:24 +0100

    Apply YAGNI principle : remove days of the week feature

--- src/statistics.js
+++ src/statistics.js
@@ -1,81 +1,26 @@
-const DaysOfTheWeek = Object.freeze({
-  SUNDAY: Symbol('Sunday'),
-  MONDAY: Symbol('Monday'),
-  TUESDAY: Symbol('Tuesday'),
-  WEDNESDAY: Symbol('Wednesday'),
-  THRUSDAY: Symbol('Thursday'),
-  FRIDAY: Symbol('Friday'),
-  SATURDAY: Symbol('Saturday'),
-});
-
 const DayNight = Object.freeze({
   DAY: Symbol('Day'),
   NIGHT: Symbol('Night'),
 });
 
-const dayToDaysOfTheWeek = (dayNumber) => (
-  DaysOfTheWeek[Object.keys(DaysOfTheWeek).find((d, i) => dayNumber === i)]);
-
-const convertDatesToDaysOfTheWeek = (dates) => (
-  dates
-    .map((date) => new Date(date).getDay())
-    .map((dayNumber) => dayToDaysOfTheWeek(dayNumber)));
-
-/* eslint-disable no-param-reassign */
-const countDaysOfTheWeek = (daysOfTheWeek) => (
-  daysOfTheWeek.reduce((count, dayOfTheWeek) => {
-    count[dayOfTheWeek] = count[dayOfTheWeek] + 1 || 1;
-    return count;
-  }, {}));
-/* eslint-enable no-param-reassign */
-
-const convertDatesToHours = (dates) => dates.map((date) => new Date(date).getHours());
+const convertISODateToLocalHour = (date) => new Date(date).getHours();
 
 const hourToDayNightKey = (hour, dayStart, dayEnd) => (
   dayStart <= hour && hour < dayEnd ? DayNight.DAY : DayNight.NIGHT);
 
 /* eslint-disable no-param-reassign */
-const countAsDayNightSlots = (hours, localDayStart, localDayEnd) => (
-  hours.reduce((slots, hour) => {
-    const key = hourToDayNightKey(hour, localDayStart, localDayEnd);
-    slots[key] = slots[key] + 1 || 1;
-    return slots;
-  }, { [DayNight.DAY]: 0, [DayNight.NIGHT]: 0 }));
-/* eslint-enable no-param-reassign */
-
-const extractDayOfTheWeekAndHour = (dateAsISO) => {
-  const date = new Date(dateAsISO);
-  return { dayOfTheWeek: dayToDaysOfTheWeek(date.getDay()), hour: date.getHours() };
-};
-
-/* eslint-disable no-param-reassign */
-const groupHoursByDaysOfTheWeek = (dates) => (
+const groupHoursByDayNightSlots = (dates, localDayStart, localDayEnd) => (
   dates.reduce((group, date) => {
-    const { dayOfTheWeek, hour } = extractDayOfTheWeekAndHour(date);
-    group[dayOfTheWeek] = group[dayOfTheWeek] || [];
-    group[dayOfTheWeek].push(hour);
-    return group;
-  }, {}));
-/* eslint-enable no-param-reassign */
-
-/* eslint-disable no-param-reassign */
-const groupHoursByDaysOfTheWeekAndDayNightSlots = (dates, localDayStart, localDayEnd) => (
-  dates.reduce((group, date) => {
-    const { dayOfTheWeek, hour } = extractDayOfTheWeekAndHour(date);
-    group[dayOfTheWeek] = group[dayOfTheWeek] || { [DayNight.DAY]: [], [DayNight.NIGHT]: [] };
+    const hour = convertISODateToLocalHour(date);
     const dayNightKey = hourToDayNightKey(hour, localDayStart, localDayEnd);
-    group[dayOfTheWeek][dayNightKey].push(hour);
+    group[dayNightKey] = group[dayNightKey] || [];
+    group[dayNightKey].push(hour);
     return group;
   }, {}));
 /* eslint-enable no-param-reassign */
 
 module.exports = {
-  DaysOfTheWeek,
   DayNight,
-  convertDatesToDaysOfTheWeek,
-  countDaysOfTheWeek,
-  convertDatesToHours,
-  countAsDayNightSlots,
-  groupHoursByDaysOfTheWeek,
-  groupHoursByDaysOfTheWeekAndDayNightSlots,
+  convertISODateToLocalHour,
+  groupHoursByDayNightSlots,
 };
--- test/statistics.test.js
+++ test/statistics.test.js
@@ -1,48 +1,20 @@
 const assert = require('assert');
 const statisticsModule = require('../src/statistics');
 
-const { DaysOfTheWeek, DayNight } = statisticsModule;
-/* eslint-disable object-curly-newline */
-const { WEDNESDAY, FRIDAY, SUNDAY, TUESDAY } = DaysOfTheWeek;
-const { DAY, NIGHT } = DayNight;
-/* eslint-enable no-param-reassign */
+const { DAY, NIGHT } = statisticsModule.DayNight;
 
 describe('Statistics module', () => {
-  it('Should return days of the week from date in iso 8601 format', () => {
+  it('Should extract local hours from ISO dates', () => {
     const dates = [
       '2023-02-01T13:45:43Z',
       '2023-02-03T14:06:02Z',
       '2023-02-05T12:41:01Z',
       '2023-02-07T09:18:28Z',
     ];
-
-    const names = statisticsModule.convertDatesToDaysOfTheWeek(dates);
-    assert.deepEqual(names, [WEDNESDAY, FRIDAY, SUNDAY, TUESDAY]);
-  });
-  it('Should count the occurences of days of the week', () => {
-    const names = [WEDNESDAY, FRIDAY, SUNDAY, WEDNESDAY, SUNDAY, SUNDAY];
-    const count = statisticsModule.countDaysOfTheWeek(names);
-    assert.equal(2, count[WEDNESDAY]);
-    assert.equal(1, count[FRIDAY]);
-    assert.equal(3, count[SUNDAY]);
-  });
-  it('Should extract local hours from dates', () => {
-    const dates = [
-      '2023-02-01T13:45:43Z',
-      '2023-02-03T14:06:02Z',
-      '2023-02-05T12:41:01Z',
-      '2023-02-07T09:18:28Z',
-    ];
-    const hours = statisticsModule.convertDatesToHours(dates);
+    const hours = dates.map((date) => statisticsModule.convertISODateToLocalHour(date));
     assert.deepEqual(hours, [14, 15, 13, 10]);
   });
-  it('Should store hours in correct day/night slots', () => {
-    const hours = [8, 9, 12, 18, 20, 23, 1, 3, 6];
-    const slots = statisticsModule.countAsDayNightSlots(hours, 8, 18);
-    assert.equal(slots[DAY], 3);
-    assert.equal(slots[NIGHT], 6);
-  });
-  it('Should group hours by days of the week', () => {
+  it('Should group hours by day/night slots', () => {
     const dates = [
       '2023-02-01T13:45:43Z',
       '2023-02-01T16:45:43Z',
@@ -55,41 +27,8 @@ describe('Statistics module', () => {
       '2023-02-07T19:18:28Z',
       '2023-02-07T22:18:28Z',
     ];
-    const groupedHours = statisticsModule.groupHoursByDaysOfTheWeek(dates);
-    assert.deepEqual(
-      Object.getOwnPropertySymbols(groupedHours),
-      [WEDNESDAY, FRIDAY, SUNDAY, TUESDAY],
-    );
-    assert.deepEqual(groupedHours[WEDNESDAY], [14, 17, 19]);
-    assert.deepEqual(groupedHours[FRIDAY], [15, 22]);
-    assert.deepEqual(groupedHours[SUNDAY], [13]);
-    assert.deepEqual(groupedHours[TUESDAY], [7, 10, 20, 23]);
-  });
-  it('Should group hours by days of the week and day/night slots', () => {
-    const dates = [
-      '2023-02-01T13:45:43Z',
-      '2023-02-01T16:45:43Z',
-      '2023-02-01T18:45:43Z',
-      '2023-02-03T14:06:02Z',
-      '2023-02-03T21:06:02Z',
-      '2023-02-05T12:41:01Z',
-      '2023-02-07T06:18:28Z',
-      '2023-02-07T09:18:28Z',
-      '2023-02-07T19:18:28Z',
-      '2023-02-07T22:18:28Z',
-    ];
-    const groupedHours = statisticsModule.groupHoursByDaysOfTheWeekAndDayNightSlots(dates, 8, 18);
-    assert.deepEqual(
-      Object.getOwnPropertySymbols(groupedHours),
-      [DaysOfTheWeek.WEDNESDAY, DaysOfTheWeek.FRIDAY, DaysOfTheWeek.SUNDAY, DaysOfTheWeek.TUESDAY],
-    );
-    assert.deepEqual(groupedHours[WEDNESDAY][DAY], [14, 17]);
-    assert.deepEqual(groupedHours[WEDNESDAY][NIGHT], [19]);
-    assert.deepEqual(groupedHours[FRIDAY][DAY], [15]);
-    assert.deepEqual(groupedHours[FRIDAY][NIGHT], [22]);
-    assert.deepEqual(groupedHours[SUNDAY][DAY], [13]);
-    assert.deepEqual(groupedHours[SUNDAY][NIGHT], []);
-    assert.deepEqual(groupedHours[TUESDAY][DAY], [10]);
-    assert.deepEqual(groupedHours[TUESDAY][NIGHT], [7, 20, 23]);
+    const groupedHours = statisticsModule.groupHoursByDayNightSlots(dates, 8, 18);
+    assert.deepEqual(groupedHours[DAY], [14, 17, 15, 13, 10]);
+    assert.deepEqual(groupedHours[NIGHT], [19, 22, 7, 20, 23]);
   });
 });
