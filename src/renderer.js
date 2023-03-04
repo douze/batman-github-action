df2092003653d5da4af167e6d12a71d6a02e6425
douze <douze@users.noreply.github.com>
2023-03-04 23:37:36 +0100

    First render using percentage circle

---
+++ src/renderer.js
@@ -0,0 +1,22 @@
+const renderAsPercentageCircle = (identity, percentageByDay) => {
+  // Circle formula: perimeter = 2*PI*radius
+  // radius = perimeter / 2 / PI
+  const svgSize = 200;
+  const diameter = svgSize * 0.9;
+  const radius = diameter / 2;
+  const x = svgSize / 2;
+  const y = svgSize * 0.05;
+  const perimeter = 2 * Math.PI * radius;
+  const strokeWith = svgSize * 0.05;
+  return `
+<svg width="${svgSize}px" height="${svgSize}px">
+  <path fill="none" stroke="#2153b1" stroke-width="${strokeWith}" stroke-dasharray="${percentageByDay * 0.01 * perimeter} ${perimeter}"
+    d="M${x} ${y} a ${radius} ${radius} 0 0 1 0 ${diameter} a ${radius} ${radius} 0 0 1 0 -${diameter}"/>
+  <path fill="none" stroke="#f18e3c" stroke-width="${strokeWith}" stroke-dasharray="0 ${percentageByDay * 0.01 * perimeter} ${perimeter}"
+    d="M${x} ${y} a ${radius} ${radius} 0 0 1 0 ${diameter} a ${radius} ${radius} 0 0 1 0 -${diameter}"/>
+  <text x="100" y="100" alignment-baseline="central" text-anchor="middle">${identity.toUpperCase()}</text>
+</svg>
+`;
+};
+
+module.exports = { renderAsPercentageCircle };
---
+++ test/renderer.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const rendererModule = require('../src/renderer');
+const batmanModule = require('../src/batman');
+
+const { Identity } = batmanModule;
+
+const expectedCircle = `
+<svg width="200px" height="200px">
+  <path fill="none" stroke="#2153b1" stroke-width="10" stroke-dasharray="452.38934211693027 565.4866776461628"
+    d="M100 10 a 90 90 0 0 1 0 180 a 90 90 0 0 1 0 -180"/>
+  <path fill="none" stroke="#f18e3c" stroke-width="10" stroke-dasharray="0 452.38934211693027 565.4866776461628"
+    d="M100 10 a 90 90 0 0 1 0 180 a 90 90 0 0 1 0 -180"/>
+  <text x="100" y="100" alignment-baseline="central" text-anchor="middle">BATMAN</text>
+</svg>
+`;
+
+describe('Renderer module', () => {
+  it('Should render circle SVG', async () => {
+    const svg = rendererModule.renderAsPercentageCircle(Identity.BATMAN.description, 80);
+    assert.equal(svg, expectedCircle);
+  });
+});
